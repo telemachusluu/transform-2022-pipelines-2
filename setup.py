1aa299e5ded870c2aafd5cdfc216702aae9deab0
Kevin Breit <kevin.breit@kevinbreit.net>
2022-03-29 08:35:36 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""
+Setup Python environment for testing
+"""
+
+from setuptools import setup
+
+setup(name="app-template-api", packages=["project"])
---
+++ tests/unit/test_app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pytest
+from project.main import app
+
+
+def test_index_route():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "Hello World!"
