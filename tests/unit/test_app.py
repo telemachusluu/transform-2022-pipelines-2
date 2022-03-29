8538fcf5d77ec56b647406cb2e599c85296b383b
Kevin Breit <kevin.breit@kevinbreit.net>
2022-03-29 12:34:04 -0500

    Make pytest work

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -7,4 +7,4 @@ from project.app import app
 def test_index_route():
     response = app.test_client().get("/")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello World!"
+    assert b"Welcome to our session!" in response.data
